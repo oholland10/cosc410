a70984f3cc4de503e2d6bb00f66af367af6eab97
Grusha Prasad <gprasad@colgate.edu>
2025-02-26 15:05:01 -0500

    adding lab5

---
+++ labs/lab05/part1_models.py
@@ -0,0 +1,63 @@
+import torch
+
+class Perceptron(torch.nn.Module):
+    """
+    Single layer perceptron with no hidden layers
+
+    """
+    def __init__(self, num_inputs):
+        super().__init__() # ensures base nn is correctly initialized
+
+        ## Implement this
+        pass
+        
+
+    def forward(self, X):
+        ## Implement this
+        pass
+
+    def loss(self, y_hat, y):
+        ## Implement this. 
+        pass
+
+class FNN_1layer(torch.nn.Module):
+    """
+    FNN with one hidden layer and non-linear activation function
+
+    """
+
+    def __init__(self, num_inputs):
+        super().__init__() # ensures base nn is correctly initialized
+
+        ## Implement this
+        pass
+        
+
+    def forward(self, X):
+        ## Implement this
+        pass
+
+    def loss(self, y_hat, y):
+        ## Implement this.
+        pass
+
+class FNN_1layer_linear(torch.nn.Module):
+    """
+    FNN with one hidden layer and linear activation function
+
+    """
+
+    def __init__(self, num_inputs):
+        super().__init__() # ensures base nn is correctly initialized
+
+        ## Implement this
+        pass
+        
+
+    def forward(self, X):
+        ## Implement this
+        pass
+
+    def loss(self, y_hat, y):
+        ## Implement this.
+        pass
---
+++ labs/lab05/part2_models.py
@@ -0,0 +1,3 @@
+import torch
+
+## Add the model classes you need for Lab 5 Part 2
