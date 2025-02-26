932d3068a9f68f86c37042719c3dbdd2da2ead70
Grusha Prasad <gprasad@colgate.edu>
2025-02-26 15:15:14 -0500

    changing class definitions

--- labs/lab05/part1_models.py
+++ labs/lab05/part1_models.py
@@ -5,7 +5,7 @@ class Perceptron(torch.nn.Module):
     Single layer perceptron with no hidden layers
 
     """
-    def __init__(self, num_inputs):
+    def __init__(self, num_inputs, num_classes):
         super().__init__() # ensures base nn is correctly initialized
 
         ## Implement this
@@ -26,7 +26,7 @@ class FNN_1layer(torch.nn.Module):
 
     """
 
-    def __init__(self, num_inputs):
+    def __init__(self, num_inputs,  num_classes, activation_function):
         super().__init__() # ensures base nn is correctly initialized
 
         ## Implement this
@@ -47,7 +47,7 @@ class FNN_1layer_linear(torch.nn.Module):
 
     """
 
-    def __init__(self, num_inputs):
+    def __init__(self, num_inputs, num_classes, activation_function):
         super().__init__() # ensures base nn is correctly initialized
 
         ## Implement this
